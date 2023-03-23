0da61f38ed8d1b977ff8f9757ec2d81437caea6f
wagner <wagner.santos@accurate.com.br>
2023-03-23 14:28:33 -0300

    Criada classe Administrador herdando FuncionarioAutenticavel

---
+++ aula_003_interface/src/Administrador.java
@@ -0,0 +1,7 @@
+public class Administrador extends FuncionarioAutenticavel {
+
+    public double getBonificacao() {
+        System.out.println("Bonificacao do Administrador");
+        return 500;
+    }
+}
