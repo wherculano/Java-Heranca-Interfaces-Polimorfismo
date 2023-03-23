045fae2570aec6406cb955cbf531998c12026f33
wagner <wagner.santos@accurate.com.br>
2023-03-23 16:21:22 -0300

    Criada classe Conta

---
+++ aula_004_interfaces/src/Conta.java
@@ -0,0 +1,66 @@
+public abstract class Conta {
+    protected double saldo;
+    private int agencia;
+    private int numero;
+
+    private static int total;
+
+    public Conta(int agencia, int numero) {
+        this.agencia = agencia;
+        this.numero = numero;
+        System.out.println("Agencia " + this.agencia + " de numero " + this.numero + " criada.");
+        Conta.total++;  // atributo da classe e nao somente da instancia
+    }
+
+    public double getSaldo() {
+        return this.saldo;
+    }
+
+    public abstract void deposita(double valor);
+
+    public boolean saca(double valor) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean transfere(double valor, Conta destino) {
+        boolean saqueOk = this.saca(valor);
+        if (saqueOk) {
+            destino.deposita(valor);
+            return true;
+        }
+        return false;
+    }
+
+    public int getAgencia() {
+        return this.agencia;
+    }
+
+    public void setAgencia(int agencia) {
+        if (agencia <= 0) {
+            System.out.println("Não é permitido valor menor ou igual a 0");
+            return;
+        }
+        this.agencia = agencia;
+    }
+
+    public int getNumero() {
+        return this.numero;
+    }
+
+    public void setNumero(int numero) {
+        if (numero <= 0) {
+            System.out.println("Não é permitido valor menor ou igual a 0");
+            return;
+        }
+        this.numero = numero;
+    }
+
+
+    public static int getTotal() {
+        return Conta.total;
+    }
+}
