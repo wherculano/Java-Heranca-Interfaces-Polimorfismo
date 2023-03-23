6dda1022f72a91475c9c036dc11d7d97010ba1a5
wagner <wagner.santos@accurate.com.br>
2023-03-23 16:20:36 -0300

    Criada classe ContaCorrente que herda Conta e implementa Tributavel

---
+++ aula_004_interfaces/src/ContaCorrente.java
@@ -0,0 +1,26 @@
+public class ContaCorrente extends Conta implements Tributavel{
+
+    public ContaCorrente(int agencia, int numero) {
+        super(agencia, numero);
+    }
+
+    @Override
+    public boolean saca(double valor) {
+        double valorComTaxa = valor + 0.10;
+        return super.saca(valorComTaxa);
+    }
+
+    @Override
+    public void deposita(double valor) {
+        if (valor <= 0) {
+            System.out.println("Não é permitido valor menor ou igual a 0");
+            return;
+        }
+        this.saldo += valor;
+    }
+
+    @Override
+    public double getValorImposto() {
+        return super.saldo * 0.01;
+    }
+}
