d69f3a869d4ed914a169103dcca78712fac28e51
wagner <wagner.santos@accurate.com.br>
2023-03-23 16:21:42 -0300

    Criada classe CalculadorImposto

---
+++ aula_004_interfaces/src/CalculadorImposto.java
@@ -0,0 +1,13 @@
+public class CalculadorImposto {
+    private double totalImpostos;
+
+    public void registra(Tributavel tributavel) {
+        double valor = tributavel.getValorImposto();
+        this.totalImpostos += valor;
+    }
+
+    public double getTotalImpostos() {
+        return totalImpostos;
+    }
+    
+}
